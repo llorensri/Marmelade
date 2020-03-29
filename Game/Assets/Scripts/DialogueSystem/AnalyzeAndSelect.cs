97828b8951c313411d3a9ef58c92d42d919975df
Diego <diego.llorensrico@gmail.com>
2020-03-29 23:54:13 +0200

    DALEBEAAAA

---
+++ Game/Assets/Scripts/DialogueSystem/AnalyzeAndSelect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyzeAndSelect : MonoBehaviour
+{
+    public DialogueChain karma_good;
+    public DialogueChain karma_bad;
+    public DialogueChain karma_neu;
+
+    public int karma_min_good;
+    public int karma_min_bad;
+
+    private void Start()
+    {
+        if (GameManagerScript.Instance.karma >= karma_min_good)
+        {
+            GetComponent<RefHandler>().SetDialogueChain(karma_good);
+        }else if(GameManagerScript.Instance.karma<= karma_min_bad)
+        {
+            GetComponent<RefHandler>().SetDialogueChain(karma_bad);
+
+        }
+        else
+        {
+            GetComponent<RefHandler>().SetDialogueChain(karma_neu);
+
+        }
+    }
+}
